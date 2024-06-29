a94b216e7b06fdc23095eb9cda396c0ae03560c2
mhmtkarakas <m.karakas32@gmail.com>
2024-06-29 16:09:26 +0300

    deleteTransaction

--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -16,8 +16,18 @@ export const GlobalContext = createContext(initialState);
 //ProviderComponent
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+  // Actions
+  const deleteTransaction = (id) => {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  };
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{ transactions: state.transactions, deleteTransaction }}
+    >
       {children}
     </GlobalContext.Provider>
   );
